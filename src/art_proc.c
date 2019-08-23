1cc6ebe304827d23525316598cfda7768f991f3f
sripad <sripad1996@gmail.com>
2019-08-23 23:59:04 +0530

    Libnpnt changes :1)Digest algo changed from sha1 to sha 256. 2)Removed Few parameter checks

--- src/art_proc.c
+++ src/art_proc.c
@@ -74,17 +74,18 @@ int8_t npnt_set_permart(npnt_s *handle, uint8_t *permart, uint16_t permart_lengt
     ret = 0;
 
     //Get Max Altitude
-    ret = npnt_get_max_altitude(handle, &handle->fence.maxAltitude);
-    if (ret < 0) {
-        return NPNT_INV_BAD_ALT;
-    }
+    // ret = npnt_get_max_altitude(handle, &handle->fence.maxAltitude);
+    // if (ret < 0) {
+    //     return NPNT_INV_BAD_ALT;
+    // }
 
     //Set Flight Params from artefact
     ret = npnt_populate_flight_params(handle);
-    if (ret < 0) {
-        handle->fence.nverts = 0;
-        return NPNT_INV_FPARAMS;
-    }
+    // if (ret < 0) {
+    //     handle->fence.nverts = 0;
+    //     printf("fence error\n");
+    //     return NPNT_INV_FPARAMS;
+    // }
     ret = 0;
     return ret;
 }
@@ -97,7 +98,7 @@ int8_t npnt_verify_permart(npnt_s *handle)
     const uint8_t* rcvd_digest_value;
     // char *test_str;
     int16_t permission_length, signedinfo_length;
-    char digest_value[20];
+    char digest_value[32];
     const uint8_t* signature = NULL;
     uint8_t* raw_signature = NULL;
     uint16_t signature_len, raw_signature_len;
@@ -164,19 +165,21 @@ int8_t npnt_verify_permart(npnt_s *handle)
     signature_len = strlen(signature);
     raw_signature = base64_decode(signature, signature_len, &raw_signature_len);
     //Check authenticity of the artifact
-    if (npnt_check_authenticity(handle, digest_value, 20, raw_signature, raw_signature_len) <= 0) {
+    if (npnt_check_authenticity(handle, digest_value, 32, raw_signature, raw_signature_len) < 0) {
         ret = NPNT_INV_AUTH;
         goto fail;
     }
 
     //Digest Canonicalised Permission Artifact
-    raw_perm_without_sign = strstr(handle->raw_permart, "<UAPermission>");
+    raw_perm_without_sign = strstr(handle->raw_permart, "<UAPermission");
     if (raw_perm_without_sign == NULL) {
+        printf("ua fails\n");
         ret = NPNT_INV_ART;
         goto fail;
     }
     permission_length = strstr(handle->raw_permart, "<Signature") - raw_perm_without_sign;
     if (permission_length < 0) {
+        printf("sig fail\n");
         ret = NPNT_INV_ART;
         goto fail;
     }
@@ -225,7 +228,7 @@ int8_t npnt_verify_permart(npnt_s *handle)
     raw_perm_without_sign = strstr(handle->raw_permart, "</Signature>") + strlen("</Signature>");
     update_sha1(raw_perm_without_sign, strlen(raw_perm_without_sign));
     final_sha1(digest_value);
-    base64_digest_value = base64_encode(digest_value, 20, &base64_digest_value_len);
+    base64_digest_value = base64_encode(digest_value, 32, &base64_digest_value_len);
     // printf("\nDigest Value: \n%s\n", base64_digest_value);
     // printf("\nDigest Value: \n%s\n", mxmlGetOpaque(mxmlFindElement(handle->parsed_permart, handle->parsed_permart, "DigestValue", NULL, NULL, MXML_DESCEND)));
     
@@ -395,27 +398,32 @@ int8_t npnt_populate_flight_params(npnt_s* handle)
     mxml_node_t *ua_detail, *flight_params;
     ua_detail = mxmlFindElement(handle->parsed_permart, handle->parsed_permart, "UADetails", NULL, NULL, MXML_DESCEND);
     if (!ua_detail) {
+        printf("ua fail\n");
         return NPNT_INV_FPARAMS;
     }
     flight_params = mxmlFindElement(handle->parsed_permart, handle->parsed_permart, "FlightParameters", NULL, NULL, MXML_DESCEND);
     if (!flight_params) {
+        printf("flight param fail\n");
         return NPNT_INV_FPARAMS;
     }
 
     handle->params.uinNo = npnt_get_attr(ua_detail, "uinNo");
     if (!handle->params.uinNo) {
+        printf("uin fail\n");
         return NPNT_INV_FPARAMS;
     }
 
-    handle->params.adcNumber = npnt_get_attr(flight_params, "adcNumber");
-    if (!handle->params.adcNumber) {
-        return NPNT_INV_FPARAMS;
-    }
+    // handle->params.adcNumber = npnt_get_attr(flight_params, "adcNumber");
+    // if (!handle->params.adcNumber) {
+    //     printf("adc fail\n");
+    //     return NPNT_INV_FPARAMS;
+    // }
 
-    handle->params.ficNumber = npnt_get_attr(flight_params, "ficNumber");
-    if (!handle->params.ficNumber) {
-        return NPNT_INV_FPARAMS;
-    }
+    // handle->params.ficNumber = npnt_get_attr(flight_params, "ficNumber");
+    // if (!handle->params.ficNumber) {
+    //     printf("fic fail\n");
+    //     return NPNT_INV_FPARAMS;
+    // }
     if (npnt_ist_date_time_to_unix_time(mxmlElementGetAttr(flight_params, "flightEndTime"), &handle->params.flightEndTime) < 0) {
         return NPNT_INV_FPARAMS;
     }
--- src/npnt_helpers.c
+++ src/npnt_helpers.c
@@ -1,5 +1,5 @@
 #include <npnt.h>
-
+#define RFM_USE_WOLFSSL 
 #ifdef RFM_USE_WOLFSSL
 // #include <wolfssl/openssl/bio.h>
 // #include <wolfssl/openssl/err.h>
@@ -21,22 +21,26 @@ static RsaKey         rsaKey;
 static RsaKey*        pRsaKey = NULL;
 int8_t npnt_check_authenticity(npnt_s *handle, uint8_t* raw_data, uint16_t raw_data_len, const uint8_t* signature, uint16_t signature_len)
 {
+        printf("wolfssl called");
     int ret = 0;
 
     if (pRsaKey == NULL) {
         /* Initialize the RSA key and decode the DER encoded public key. */
         FILE *fp = fopen("dgca_pubkey.pem", "r");
         if (fp == NULL) {
+            printf("pub key read fail");
             return -1;
         }
         fseek(fp, 0L, SEEK_END);
         uint32_t sz = ftell(fp);
         rewind(fp);
         if (sz == 0) {
+            printf("size error");
             return -1;
         }
         uint8_t *filebuf = (uint8_t*)malloc(sz);
         if (filebuf == NULL) {
+            printf("filebuff error\n");
             return -1;
         }
         uint32_t idx = 0;
@@ -47,18 +51,22 @@ int8_t npnt_check_authenticity(npnt_s *handle, uint8_t* raw_data, uint16_t raw_d
 
         if (ret == 0) {
             ret = wc_InitRsaKey(&rsaKey, 0);
+            printf("Tried init. rsa key\n");
         }
         if (ret == 0) {
             ret = wc_RsaPublicKeyDecode(converted->buffer, &idx, &rsaKey, converted->length);
+            printf("Tried decode. rsa key\n");
         }
         if (ret == 0) {
             pRsaKey = &rsaKey;
+            printf("Tried pointing rsa key\n");
         }
         free(filebuf);
         close(fp);
     }
 
     if (ret < 0) {
+            printf("ret error\n");
         return -1;
     }
     uint8_t* decSig = NULL;
@@ -68,16 +76,27 @@ int8_t npnt_check_authenticity(npnt_s *handle, uint8_t* raw_data, uint16_t raw_d
         decSigLen = wc_RsaSSL_VerifyInline(signature, signature_len,
                                            &decSig, pRsaKey);
         if ((int)decSigLen < 0) {
+            
             ret = (int)decSigLen;
+            printf("decSigLen error  %d\n", ret);
         }
     }
+    uint8_t enchash[64];
+    raw_data_len = wc_EncodeSignature(enchash, raw_data, raw_data_len, SHA256h);
 
     /* Check the decrypted result matches the encoded digest. */
     if (ret == 0 && decSigLen != raw_data_len)
+    {
+        printf("sig len: %d\n " , signature_len );
+        printf("data length error %d  %d\n", decSigLen , raw_data_len); 
         ret = -1;
-    if (ret == 0 && XMEMCMP(raw_data, decSig, decSigLen) != 0)
+    }
+    if (ret == 0 && XMEMCMP(enchash, decSig, decSigLen) != 0)
+    {
         ret = -1;
-
+        printf("loop complete  %d\n",ret);
+    }
+    printf("finifhed  %d\n",ret);
     return ret;
 }
 static Sha sha;
@@ -117,31 +136,38 @@ static EVP_PKEY *dgca_pkey = NULL;
 static EVP_PKEY_CTX *dgca_pkey_ctx;
 int8_t npnt_check_authenticity(npnt_s *handle, uint8_t* hashed_data, uint16_t hashed_data_len, const uint8_t* signature, uint16_t signature_len)
 {
+        printf("openssl called ");
     if (!handle || !raw_data || !signature) {
+        printf("raw_data or error");
         return -1;
     }
     if (dgca_pkey == NULL) {
         FILE *fp = fopen("dgca_pubkey.pem", "r");
         if (fp == NULL) {
+            printf("pub key error");
             return -1;
         }
         dgca_pkey = PEM_read_PUBKEY(fp, NULL, NULL, NULL);
     }
     dgca_pkey_ctx = EVP_PKEY_CTX_new(dgca_pkey, ENGINE_get_default_RSA());
     if (!dgca_pkey_ctx) {
+        printf("EVP_PKEY_CTX_new error");
         return -1;
     }
     int ret = 0;
     if (EVP_PKEY_verify_init(dgca_pkey_ctx) <= 0) {
         ret = -1;
+        printf("EVP_PKEY_verify_init error");
         goto fail;
     }
     if (EVP_PKEY_CTX_set_rsa_padding(dgca_pkey_ctx, RSA_PKCS1_PADDING) <= 0) {
         ret = -1;
+        printf("EVP_PKEY_padding");
         goto fail;
     }
     if (EVP_PKEY_CTX_set_signature_md(dgca_pkey_ctx, EVP_sha1()) <= 0) {
         ret = -1;
+        printf("EVP_PKEY_signature_md");
         goto fail;
     }
 
@@ -150,6 +176,7 @@ int8_t npnt_check_authenticity(npnt_s *handle, uint8_t* hashed_data, uint16_t ha
 
 fail:
     EVP_PKEY_CTX_free(dgca_pkey_ctx);
+    printf("fail statement");
     return ret;
 }
 #endif
